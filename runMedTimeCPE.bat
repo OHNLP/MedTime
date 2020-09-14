2232fd685fba46d574d78bcb6a98d1d2a3d443a1
Andrew Wen <Wen.Andrew@mayo.edu>
2020-09-14 09:17:45 -0500

    Fix build structure inconsistencies in dist version

--- runMedTimeCPE.bat
+++ runMedTimeCPE.bat
@@ -1,2 +1,2 @@
-java -Xms512M -Xmx2048M -cp resources;desc;descsrc;medtaggerdescsrc;MedTime-1.0.2.jar org.apache.uima.tools.cpm.CpmFrame 
+java -Xms512M -Xmx2048M -cp resources;MedTime.jar org.apache.uima.tools.cpm.CpmFrame
 
